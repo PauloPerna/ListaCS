0382f5c357cb088b60bc735529aa360a812b3186
Paulo Perna <paulo.perna.rocha@gmail.com>
2022-11-28 11:23:46 -0300

    Exercicio 12

---
+++ Exercicio12.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nivel2{
+    public class Exercicio12{
+        static void Main(string[] args){
+            Console.WriteLine("Digite o valor de x:");
+            decimal.TryParse(Console.ReadLine(), out decimal x);
+            x = x > 0? x : -x;
+            Console.WriteLine($"O módulo de x é {x}");
+        }
+    }
+}
